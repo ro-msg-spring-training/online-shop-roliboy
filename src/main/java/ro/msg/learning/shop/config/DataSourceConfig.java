3bf585b1a891dad1f5667c3ccb8e7bcac794cccb
Nagy Roland <roliboy@protonmail.com>
2021-07-22 11:59:22 +0300

    create separate class for configuring the data source based on profile

---
+++ src/main/java/ro/msg/learning/shop/config/DataSourceConfig.java
@@ -0,0 +1,34 @@
+package ro.msg.learning.shop.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Profile("prod")
+    @Bean
+    public DataSource getDevDataSource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:~/Documents/h2/shop;DB_CLOSE_ON_EXIT=FALSE;DATABASE_TO_UPPER=false");
+        dataSourceBuilder.username("");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
+
+    @Profile("test")
+    @Bean
+    public DataSource getProdDataSource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:test;DB_CLOSE_ON_EXIT=FALSE;DATABASE_TO_UPPER=false");
+//        dataSourceBuilder.url("jdbc:h2:~/Documents/h2/testshop;DB_CLOSE_ON_EXIT=FALSE;DATABASE_TO_UPPER=false");
+        dataSourceBuilder.username("");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
+}
