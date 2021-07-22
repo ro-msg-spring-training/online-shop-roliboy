eb6499a7d490d16029ad283af2cd05980c06b8da
Nagy Roland <roliboy@protonmail.com>
2021-07-22 13:31:30 +0300

    implement basic authentication and form authentication

---
+++ src/main/java/ro/msg/learning/shop/config/BasicAuthenticationConfig.java
@@ -0,0 +1,35 @@
+package ro.msg.learning.shop.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Profile("with-basic")
+@Configuration
+@EnableWebSecurity
+public class BasicAuthenticationConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private MyBasicAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}admin").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/h2-console").hasRole("ADMIN")
+                .antMatchers("/api/users").hasRole("ADMIN")
+                .antMatchers("/**").permitAll()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint);
+    }
+}
---
+++ src/main/java/ro/msg/learning/shop/config/MyBasicAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package ro.msg.learning.shop.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//TODO: put this file somewhere else
+@Component
+public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+    @Override
+    public void commence(
+            HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+            throws IOException {
+        response.addHeader("WWW-Authenticate", "Basic realm=\"" + getRealmName() + "\"");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("HTTP Status 401 - " + authEx.getMessage());
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        setRealmName("chiar el");
+        super.afterPropertiesSet();
+    }
+}
